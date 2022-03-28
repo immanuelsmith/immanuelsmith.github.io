5dd554ea9a1e118f457dbeff774cfa057286b266
immanuelsmith <ismith.user@gmail.com>
2022-03-28 18:45:55 -0400

    Add files via upload

---
+++ email.js
@@ -0,0 +1,13 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
+ */
+
+function tap(){
+    var email1="immanuel";
+    var e2="gmail.com"
+    var z3=".smith22"
+    document.getElementById("hiddenemail").innerHTML = "Please email me at " + email1 + z3 + "@" +e2;
+    window.open("mailto:" + email1 + z3 + "@" +e2, "_blank");
+}
+
